d3bbd459200dca756eee3be0ecee0584d080ebce
Shegzzy <miksegun1996@gmail.com>
2023-06-21 23:33:07 +0100

    redeploy

--- eStore/settings.py
+++ eStore/settings.py
@@ -68,24 +68,26 @@ WSGI_APPLICATION = "eStore.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "railway",
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#         "URL": os.getenv("DB_URL"),
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "verceldb",
+        "POSTGRES_PRISMA_URL": "postgres://default:o16LpOCERwkr@ep-empty-limit-800726-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
+        "POSTGRES_URL_NON_POOLING": "postgres://default:o16LpOCERwkr@ep-empty-limit-800726.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+        "USER": os.getenv("DB_USER"),
+        "POSTGRES_PASSWORD": os.getenv("DB_PASSWORD"),
+        "POSTGRES_HOST": os.getenv("DB_HOST"),
+        # "PORT": os.getenv("DB_PORT"),
+        "POSTGRES_URL": os.getenv("DB_URL"),
+    }
+}
 
 
 # Password validation
