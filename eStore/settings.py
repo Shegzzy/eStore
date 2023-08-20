c41d8d773fa0d912fc720546cc562d65efa2a437
Segun <miksegun1996@gmail.com>
2023-08-20 22:07:35 +0100

    added port to the db

--- eStore/settings.py
+++ eStore/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
-        # "PORT": config("DB_PORT"),
+        "PORT": config("DB_PORT"),
         "URL": config("DB_URL"),
     }
 }
